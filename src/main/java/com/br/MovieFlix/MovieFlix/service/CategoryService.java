d163bec4d6be06da41b1dc3009d979f18c6d76be
Alan <aalantqr@gmail.com>
2026-04-28 17:07:03 -0300

    feat: create Category service

--- src/main/java/com/br/MovieFlix/MovieFlix/service/CategoryService.java
+++ src/main/java/com/br/MovieFlix/MovieFlix/service/CategoryService.java
@@ -1,4 +1,39 @@
 package com.br.MovieFlix.MovieFlix.service;
 
+import com.br.MovieFlix.MovieFlix.entity.Category;
+import com.br.MovieFlix.MovieFlix.repository.CategoryRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
 public class CategoryService {
+
+    private final CategoryRepository categoryRepository;
+
+    public List<Category> findAll() {
+        return categoryRepository.findAll();
+    }
+
+    public Category findById(Long id) {
+        return categoryRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Category not found with id"));
+    }
+
+    public Category alter(Long id, Category category) {
+        return categoryRepository.save(category);
+    }
+
+    public Category create(Category category) {
+        return categoryRepository.save(category);
+    }
+
+    public void delete(Long id) {
+        categoryRepository.deleteById(id);
+
+    }
+
+
 }
