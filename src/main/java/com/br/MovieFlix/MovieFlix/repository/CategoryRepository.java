c21f3312b99ce5f00d5590086686510727cf48cf
Alan <aalantqr@gmail.com>
2026-04-28 17:06:56 -0300

    feat: create Category repository

--- src/main/java/com/br/MovieFlix/MovieFlix/repository/CategoryRepository.java
+++ src/main/java/com/br/MovieFlix/MovieFlix/repository/CategoryRepository.java
@@ -1,4 +1,9 @@
 package com.br.MovieFlix.MovieFlix.repository;
 
-public interface CategoryRepository {
+import com.br.MovieFlix.MovieFlix.entity.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
 }
