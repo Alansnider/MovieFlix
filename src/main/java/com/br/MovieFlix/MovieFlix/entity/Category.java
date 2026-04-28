5ec3cd871a8aa411d3f12364d0e5f57525eebde7
Alan <aalantqr@gmail.com>
2026-04-28 17:06:50 -0300

    feat: create Category entity

--- src/main/java/com/br/MovieFlix/MovieFlix/entity/Category.java
+++ src/main/java/com/br/MovieFlix/MovieFlix/entity/Category.java
@@ -1,4 +1,22 @@
 package com.br.MovieFlix.MovieFlix.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "category")
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 100, nullable = false)
+    private String name;
+
+
+
+
 }
