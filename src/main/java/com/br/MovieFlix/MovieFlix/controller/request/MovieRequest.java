f3264819fc9cc68cfc96fcf8131f258178f865df
Alan <aalantqr@gmail.com>
2026-05-03 18:38:35 -0300

    feat: improve movie request structure

--- src/main/java/com/br/MovieFlix/MovieFlix/controller/request/MovieRequest.java
+++ src/main/java/com/br/MovieFlix/MovieFlix/controller/request/MovieRequest.java
@@ -4,10 +4,17 @@ import java.time.LocalDate;
 import java.util.List;
 
 
-public record MovieRequest(String title,
+public record MovieRequest(
+
+        String title,
+
          String description,
+
          LocalDate releaseDate,
+
          double rating,
+
          List<Long> categories,
+
          List<Long> streamings) {
 }
