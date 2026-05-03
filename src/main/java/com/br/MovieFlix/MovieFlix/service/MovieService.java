3c731d23d67fb37d48ce81f1336b8657c51ad41b
Alan <aalantqr@gmail.com>
2026-05-03 18:38:49 -0300

    feat: update movie service business rules

--- src/main/java/com/br/MovieFlix/MovieFlix/service/MovieService.java
+++ src/main/java/com/br/MovieFlix/MovieFlix/service/MovieService.java
@@ -3,11 +3,12 @@ package com.br.MovieFlix.MovieFlix.service;
 import com.br.MovieFlix.MovieFlix.controller.request.CategoryRequest;
 import com.br.MovieFlix.MovieFlix.controller.response.CategoryResponse;
 import com.br.MovieFlix.MovieFlix.entity.Category;
+import com.br.MovieFlix.MovieFlix.mapper.MovieMapper;
 import com.br.MovieFlix.MovieFlix.repository.MovieRepository;
-import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -15,39 +16,17 @@ import java.util.List;
 public class MovieService {
 
     private final MovieRepository movieRepository;
-
-    public List<CategoryResponse> list(){
-        return movieRepository.findAll()
-                .stream()
-                .map(categoryMapper::toCategoryResponse)
-                .toList();
-    }
+    private final MovieMapper movieMapper;
+    private final CategoryService categoryService;
+    private final StreamingService streamingService;
 
 
-    public CategoryResponse findById(Long id) {
-        Category category = movieRepository.findById(id)
-                .orElseThrow(() -> new EntityNotFoundException("Category not found with id: " + id));
-        return categoryMapper.toCategoryResponse(category);
-    }
 
-    public CategoryResponse update(CategoryRequest categoryRequest, Long id) {
-        Category existing = movieRepository.findById(id)
-                .orElseThrow(() -> new EntityNotFoundException("Category not found with id: " + id));
-        existing.setName(categoryRequest.name());
-        Category update = movieRepository.save(existing);
-        return categoryMapper.toCategoryResponse(update);
-    }
 
-    public CategoryResponse create(CategoryRequest categoryRequest) {
-        Category category = movieRepository.save(categoryMapper.toCategory(categoryRequest));
-        return categoryMapper.toCategoryResponse(category);
-    }
 
-    public void deleteById(Long id) {
-        Category category = movieRepository.findById(id)
-                .orElseThrow(() -> new EntityNotFoundException("Category not found with id: " + id));
-        categoryRepository.delete(category);
-    }
+
+
+
 
 
 
